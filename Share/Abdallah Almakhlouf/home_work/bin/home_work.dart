45082db6a2b5021a5740598037876a2064048028
Abdullah Almakhlouf <126766599+Mr000kh@users.noreply.github.com>
2023-03-12 23:55:01 +0300

    Add files via upload

---
+++ Share/Abdallah Almakhlouf/home_work/bin/home_work.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void main(){
+// print("q1");
+//
+// for(int i=1;i<=10;i++) {
+//   print(i);
+// }
+// print("Q2");
+// int num1,num2;
+// int sum=0;
+//print("enter tow number");
+// num1=num.parse(stdin.readLineSync());
+// num2=num.parse(stdin.readLineSync());
+// for(;num1<=num2;num1++){
+//   sum+=num1;
+// }
+// print(sum);
+ // print("Q3");
+// print("enter your number");
+// int mul;
+// int number=num.parse(stdin.readLineSync());
+// for( int i=1;i<=12;i+=1){
+//   mul=number*i;
+//   print("$number * $i =$mul ");
+// }
+//
+// print("Q5");
+//
+// int wanted=1234;
+// int pass;
+// do{
+//   print("enter your num");
+//   pass=num.parse(stdin.readLineSync());
+//
+// }while(pass!=wanted);
+// print("correct pass");
+
+
+
+
+
+
+}
