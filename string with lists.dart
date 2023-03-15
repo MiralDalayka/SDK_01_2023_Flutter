91742b20bae26d5b6e41a9e6ec825f5a0d521eb8
ALISOHEELHAYAJNEH <42132463+ALISOHEELHAYAJNEH@users.noreply.github.com>
2023-03-15 11:50:13 -0700

    Add files via upload

---
+++ string with lists.dart
@@ -0,0 +1,169 @@
+
+
+
+
+
+
+
+
+
+
+import 'dart:io';
+
+ void main(){
+
+  // q1
+      /* var mydictionary=["fh","am","i","ra","lkk","i","fgf","jdj","ds"];
+       var longestwords=[];
+
+            if(mydictionary.isNotEmpty) {
+              mydictionary.sort((a, b) => b.length.compareTo(a.length));
+               longestwords.add(mydictionary[0]);
+               for (int index =1 ; index <mydictionary.length; index++) {
+                 if (mydictionary[index].length == mydictionary[0].length) {
+                   longestwords.add(mydictionary[index]);
+                 }
+               }
+             }
+
+
+       print(longestwords);
+*/
+                           //Q2 verify please
+
+                   // var zeroSum=[4,3,2,3,-8,6,7,-3,-5,12,-4,-6,-2];
+                   // int sum=0;
+                   // int y=0;
+                   // int x=0;
+                   // bool found=false;
+                   //
+                   //         outerloop:
+                   //         for ( x = 0; x < zeroSum.length; x++) {
+                   //
+                   //           for ( y = x; y < zeroSum.length; y++) {
+                   //
+                   //             sum = zeroSum[y] + sum;
+                   //
+                   //             if (sum == 0) {
+                   //                found=true;
+                   //               break outerloop;
+                   //             }
+                   //           }
+                   //
+                   //
+                   //           sum = 0;
+                   //
+                   //
+                   //         }
+                   //         if(found ==true) {
+                   //           print("the position of range where the sum equals 0 is [${x + 1} ,${y +
+                   //               1}]");
+                   //         }
+                   //         else{
+                   //           print("not found");
+                   //         }
+
+                             // q3
+
+                   // var word="abcdefghijklmnopqrstxyz";
+                   // bool isUnique=true;
+                   // int countDuplicate=0;
+                   // int x=0;
+                   // int y=0;
+                   // outerloop:
+                   // for( x=0;x<word.length;x++){
+                   //   for( y=0;y<word.length;y++) {
+                   //     if (word[x] == word[y]) {
+                   //       countDuplicate++;
+                   //       if (countDuplicate > 1) {
+                   //         isUnique = false;
+                   //         break outerloop;
+                   //       }
+                   //
+                   //
+                   //     }
+                   //
+                   //   }
+                   //   countDuplicate = 0;
+                   // }
+                   //      if(isUnique==true){
+                   //        print("all character are unique");
+                   //      }
+                   //      else{
+                   //            print("some charcter are duplicate");
+                   //      }
+
+                           //Q4
+
+                      // var list1=[1,2,3,4];
+                      // var list2=[5,8,9,9,9];
+                      // list1.addAll(list2);
+                      // print(list1);
+
+
+                         //  Q5
+
+                      /*var list=[1,2,5,7,8,9,9,2,3,1];
+
+                      var convertToSet=list.toSet();
+
+                      print(convertToSet.length);*/
+
+
+                       // Q6
+
+                        /*var list=[3,4,5,6,22,1];
+                       int ?elementToRemove=int.parse(stdin.readLineSync()!);
+                       list.removeAt(elementToRemove);
+                       print(list);*/
+
+
+
+
+   //Q7
+
+   // var str="abavb";
+   // int i=0;
+   // for ( i = 0; i < str.length; i++) {
+   //
+   //   if (str.indexOf(str[i]) == str.lastIndexOf(str[i])) {
+   //
+   //       break;
+   //   }
+   // }
+   //
+   //  print(i);
+
+
+
+
+                             //Q8
+                     // var list1=[1,2,3,4];
+                     // var list2=[2,5,7,8];
+                     //
+                     // list1.addAll(list2);
+                     //  list1.sort();
+                     // print(list1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                         }
+
+
+
