f09ecc407396333fdb66ba9e4b8969b1caa6e0d0
abedalbaz <107196829+abedalbaz@users.noreply.github.com>
2023-03-20 14:13:23 +0300

    Update task.dart

--- Share/Abdullah Abed Alsamad/task.dart
+++ Share/Abdullah Abed Alsamad/task.dart
@@ -18,3 +18,47 @@ void main() {
   }
 }
 //--------
+import 'dart:io';
+
+void main() {
+  // int a, s, d;
+  // a = num.parse(stdin.readLineSync());
+  // s = num.parse(stdin.readLineSync());
+  // d = num.parse(stdin.readLineSync());
+  // if (a > s) {
+  //   if (a > d)
+  //     print(a);
+  //   else
+  //     print(d);
+  // } else {
+  //   if (s > d)
+  //     print(s);
+  //   else
+  //     print(d);
+  // }
+  int c;
+  c = num.parse(stdin.readLineSync());
+  if (c < 0) {
+    print("0<then Freezing weather");
+  } else {
+    if (c > 0 && c < 10) {
+      print("0-10 then Very Cold weather");
+    } else {
+      if (c > 10 && c < 20) {
+        print("10-20 then Cold weather");
+      } else {
+        if (c > 20 && c < 30) {
+          print("20-30 then Normal in Temp");
+        } else {
+          if (c > 30 && c < 40) {
+            print("30-40 then Its Hot");
+          } else {
+            if (c > 40) {
+              print(" >=40 then Its Very Hot");
+            }
+          }
+        }
+      }
+    }
+  }
+}
