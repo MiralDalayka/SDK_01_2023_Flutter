94ad9ba1d6c4f3b2441a31e8eb896bc8310b7806
Abdullah Almakhlouf <126766599+Mr000kh@users.noreply.github.com>
2023-03-27 12:17:21 +0300

    Add files via upload

---
+++ Share/Abdallah Almakhlouf/oop1.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+// class Human{
+//   String city;
+//   String nationality;
+//   Human({String n="*",String m="**"}){
+//     city=n;
+//     nationality=m;
+//   }
+//   void info(){
+//     print('you are from $city');
+//     print('you are from $nationality');
+//   }
+//
+// }
+// void main(){
+//   var city=stdin.readLineSync();
+//    var nationality=stdin.readLineSync();
+//   Human x=Human(n: city,m: nationality );
+// x.info();
+//   // x.city=stdin.readLineSync();
+//   // print('enter your nationaltey');
+//   // x.nationality=stdin.readLineSync();
+//   // x.info();
+//
+//
+// }
+// class vehicle{
+//   int speed=0;
+//   int passenger=1;
+//   String fuel;
+//   void go(){
+//     print("object");
+// }
+// void stop(){
+//     print("object");
+//
+// }
+// void changedirction(){
+//
+// }
+// }
+// class car extends vehicle{
+//   String model;
+//   int doors;
+//   String atomaker;
+//   _radio(){
+//     print("no body touch my radio");
+//   }
+//   windishieldwiper(){
+//     print("0");
+//
+//   }
+//   ch
+// }
