f8734b809d6d4b55846181b8fafc27e8d0f17a82
Abdullah Almakhlouf <126766599+Mr000kh@users.noreply.github.com>
2023-06-03 15:18:13 +0300

    Add files via upload

--- Share/Abdallah Almakhlouf/formuitask.dart
+++ Share/Abdallah Almakhlouf/formuitask.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
 class Form1 extends StatefulWidget {
   const Form1({Key? key}) : super(key: key);
 
@@ -8,6 +12,8 @@ class Form1 extends StatefulWidget {
 }
 
 class _Form1State extends State<Form1> {
+  final formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,110 +24,131 @@ class _Form1State extends State<Form1> {
       body: SingleChildScrollView(
         child: Center(
           child: Form(
+            key: formkey,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.network('https://ouch-cdn2.icons8.com/MMkWzNH-f2dqw5zJHjQMuAyF4vQV8JeAhMvLpHX22Gc/rs:fit:256:224/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9zdmcvMjA1/L2UzYjk0MGZmLTZl/ZjQtNGEwYi04N2M0/LTBiNzBhOGU4ZWFk/ZC5zdmc.png'),
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 5,
-                        ),
-                        borderRadius: BorderRadius.circular(20),
-
-
-                      ),
-                      labelText: 'email' ,
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurpleAccent,
-                          width: 5,
-                        ),
-                        borderRadius: BorderRadius.circular(20),
-
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 250,
+                      height: 250,
+                      child: Lottie.network(
+                          'https://assets4.lottiefiles.com/packages/lf20_llbjwp92qL.json')),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
                         enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.blue,
-                              width: 5,
-                            ),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 5,
+                          ),
                           borderRadius: BorderRadius.circular(20),
-
                         ),
-                      labelText: "password",
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.deepPurpleAccent,
-                          width: 5,
+                        labelText: 'email',
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 5,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                        borderRadius: BorderRadius.circular(20),
-
                       ),
+                      validator: (email) {
+                        {
+                          if (email!.isEmpty) return "required";
+                        }
+
+                        {
+                          if (emailvaliedation(email)) return 'wrong email';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (pass) {
+                        if (pass!.isEmpty) return "required";
+                        return null;
+                      },
+                      decoration: InputDecoration(
                         enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.blue,
-                              width: 5,
-                            ),borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 5,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
                         ),
-                      labelText: 'conform password',
-                      focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.deepPurpleAccent,
-                        width: 5,
+                        labelText: "password",
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 5,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
-                      borderRadius: BorderRadius.circular(20),
-
-                    ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(onPressed: () { ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Sucssful Rigester',style: TextStyle(fontSize: 15),)),) ;
-                  }, child: Text('Registeration'),
-                    style: TextButton.styleFrom(
-
-
-                        backgroundColor: Colors.blue,
-
-                        textStyle: TextStyle(
-                          fontSize: 30,
-
-                        )
-
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (conpass) {
+                        if (conpass!.isEmpty) return "required";
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 5,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        labelText: 'conform password',
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.deepPurpleAccent,
+                            width: 5,
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
                     ),
-
                   ),
-                ),
-
-              ],
-          ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formkey.currentState!.validate()) {
+                          log('data is valied');
+                        } else {
+                          log('error');
+                        }
+                      },
+                      child: Text('Registeration'),
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          textStyle: TextStyle(
+                            fontSize: 30,
+                          )),
+                    ),
+                  ),
+                ],
+              ),
             ),
-
           ),
         ),
       ),
     );
+  }
+
+  bool emailvaliedation(String email) {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(email);
   }
 }
