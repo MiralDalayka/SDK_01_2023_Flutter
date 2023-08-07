ee9282a7282d8d096db4d591121c9d562c34efb2
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-08-07 15:05:22 +0300

    Delete Code/Dart/sdk_01_2023/lib/State_managment/example_2 directory

--- Code/Dart/sdk_01_2023/lib/State_managment/example_2/Route/const_route.dart
+++
@@ -1,6 +0,0 @@
-
-const String welcomePageRoute = '/';
-const String logInPageRoute = '/signIn';
-const String signUpPageRoute = '/SharedPrefsRegisterPage';
-const String homePageRouteShared = '/home';
-const String unDefinedRoute = '/unDefined';
