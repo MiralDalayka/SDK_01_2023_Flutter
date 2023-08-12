1a40f4261a433d97e41c6c09fdf7b3907dfd1654
MiralDalayka <89144753+MiralDalayka@users.noreply.github.com>
2023-08-12 13:19:31 +0300

    Update main.dart

--- Code/Dart/sdk_01_2023/lib/main.dart
+++ Code/Dart/sdk_01_2023/lib/main.dart
@@ -2,25 +2,77 @@ import 'package:flutter/material.dart';
 
 import 'Navigation4/Router/const_routers.dart';
 import 'Navigation4/Router/router_class.dart';
-
-void main() {
+//fire base main
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Prefs.init();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RealTimeDatabaseApp extends StatelessWidget {
+  const RealTimeDatabaseApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: realtimeProvider.PostProvider())
+      ],
+      child: MaterialApp(
+        onGenerateRoute: realtime.MyRouter.generateRoute,
+        initialRoute: realTimeRoute.splashRote,
+//       home: AddEditPostScreen(),
       ),
-      // home: Page1(),
-      initialRoute: page1Route,
-      onGenerateRoute: MyRouter.generateRoute,
     );
   }
+}
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       // home: Page1(),
+//       initialRoute: page1Route,
+//       onGenerateRoute: MyRouter.generateRoute,
+//     );
+//   }
+// }
+
+
+
+//multi provider main
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(providers: [
+//       ChangeNotifierProvider(
+//         create: (BuildContext context) => Model1(),
+//       ),
+//       ChangeNotifierProvider(
+//         create: (BuildContext context) => Model2(),
+//       ),
+//     ],
+//       child:  MaterialApp(
+//           title: 'Flutter Demo',
+//           debugShowCheckedModeBanner: false,
+//           theme: ThemeData(
+//             primarySwatch: Colors.blue,
+//           ),
+//           home: TestView(),
+//           // initialRoute: page1Route,
+//           // onGenerateRoute: MyRouter.generateRoute,
+//         )
+//     );
+//   }
 }
